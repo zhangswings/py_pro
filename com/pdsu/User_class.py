d8fc3ead8233e9a60ed78024b27ae258bf6326f6
zhangswings <beforenight@163.com>
2016-01-11 09:45:31 +0800

    Python的class允许定义许多定制方法，可以让我们非常方便地生成特定的类。
    本节介绍的是最常用的几个定制方法，还有很多可定制的方法，请参考Python的官方文档。

--- com/pdsu/User_class.py
+++ com/pdsu/User_class.py
@@ -290,3 +290,37 @@ Python的class允许定义许多定制方法，可以让我们非常方便地生
 [Python的官方文档]
 http://docs.python.org/2/reference/datamodel.html#special-method-names
 '''
+
+
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return 'Student object (name=%s)' % self.name
+    __repr__ = __str__
+
+s=Student('Tomcat')
+s
+print s
+'''
+__iter__
+
+如果一个类想被用于for ... in循环，类似list或tuple那样，就必须实现一个__iter__()方法，该方法返回一个迭代对象，然后，Python的for循环就会不断调用该迭代对象的next()方法拿到循环的下一个值，直到遇到StopIteration错误时退出循环。
+
+我们以斐波那契数列为例，写一个Fib类，可以作用于for循环：
+'''
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1 # 初始化两个计数器a，b
+
+    def __iter__(self):
+        return self # 实例本身就是迭代对象，故返回自己
+
+    def next(self):
+        self.a, self.b = self.b, self.a + self.b # 计算下一个值
+        if self.a > 100000: # 退出循环的条件
+            raise StopIteration();
+        return self.a # 返回下一个值
+
+for n in Fib():
+    print n
